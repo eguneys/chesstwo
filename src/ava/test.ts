b894f8ef6e11bd1086fff9a29f1e5d079c9a47d4
Emre Guneyler <eguneys@hotmail.com>
2022-01-01 21:19:51 +0000

    pawn pushes

--- src/ava/test.ts
+++ src/ava/test.ts
@@ -2,13 +2,13 @@ import test from 'ava';
 
 import { 
   slide_uci,
-  uci_pos, rays, board_slides, initial_situation } from '../types'
+  situation_moves,
+  uci_pos, rays, initial_situation } from '../types'
 
 test('rays', t => {
   t.log(rays['q'].get(uci_pos('a8')!))
 })
 
 test.only('situation', t => {
-  let { board } = initial_situation
-  t.log(board_slides(board).get(uci_pos('a8')!)?.map(slide_uci))
+  t.log(situation_moves(initial_situation).byorig)
 })
--- src/types.ts
+++ src/types.ts
@@ -57,32 +57,81 @@ export type Situation = {
   turn: Color
 }
 
+export type ActionType = 'slide' | 'pawnpush' | 'pawncapture' | 'pawnpromote' | 'castle'
+
+export type HasAction = {
+  action: ActionType
+}
 export type HasOrigDest = {
   orig: Pos,
   dest: Pos
 }
 
-export type Slide = HasOrigDest & {
+export type HasValidTurn = {
+  valid_turn: boolean,
+  valid_king: boolean,
+  valid_ok: boolean
+}
+
+export type HasBlocks = {
   blocks: Array<Pos>
+}
+
+export type HasCapture = {
   capture?: Pos
 }
 
-export type PawnPush = HasOrigDest & {
-  blocks: Array<Pos>
-}
-export type PawnCapture = HasOrigDest & {
-  capture: Pos
+export type IsAction = HasAction & HasOrigDest & HasValidTurn & {
+
 }
 
-export type PawnPromote = HasOrigDest & {
-  blocks: Array<Pos>
+export type Slide = IsAction & HasBlocks & HasCapture & {
+  action: 'slide'
+}
+
+export type PawnPush = IsAction & HasBlocks & {
+  action: 'pawnpush'
+}
+export type PawnCapture = IsAction & HasCapture & {
+  action: 'pawncapture'
+}
+
+export type PawnPromote = IsAction & HasBlocks & {
+  action: 'pawnpromote'
   to: PromotableRole
 }
 
-export type Castle = HasOrigDest & {
+export type Castle = IsAction & HasBlocks & {
+  action: 'castle'
   orig_rook: Pos,
   dest_rook: Pos,
-  blocks: Array<Pos>
+}
+
+export type PosActions<A extends HasAction> = {
+  byorig: PosMap<Array<A>>,
+  bydest: PosMap<Array<A>>,
+  byblocks: PosMap<Array<A>>
+  bycapture: PosMap<Array<A>>
+}
+
+
+export function isSlide(_: HasAction): _ is Slide {
+  return _.action === 'slide'
+}
+
+export const pawn_push2_ranks: ColorMap<Rank> = {
+  w: 2,
+  b: 7
+}
+
+export const pawn_promote_ranks: ColorMap<Rank> = {
+  w: 8,
+  b: 1
+}
+
+export const pawn_push: ColorMap<Dir> = {
+  w: [0, 1],
+  b: [0, -1]
 }
 
 export const knight: Array<Dir> = [[-2, 1], [2, 1], [-2, -1], [2, -1],
@@ -94,7 +143,6 @@ export const king: Array<Dir> = [...queen]
 
 export const long_projection: Array<Projection> = [1, 2, 3, 4, 5, 6, 7]
 export const short_projection: Array<Projection> = [1]
-
 
 export const raydirs: RayRoleMap<[Array<Dir>, Array<Projection>]> = {
   n: [knight, short_projection],
@@ -143,6 +191,11 @@ export const rays: RayRoleMap<PosMap<Array<Ray>>> = objmap(raydirs,
       make_ray(dir, pos, projection) || []))
   ))
 
+export function pawnpush_rays(orig: Pos, color: Color): Array<Ray> {
+  let projections: Array<Projection> = pos_rank(orig) === pawn_push2_ranks[color] && [1, 2] || [1]
+  let dir = pawn_push[color]
+  return projections.flatMap(projection => make_ray(dir, orig, projection) || [])
+}
 
 export function make_ray(dir: Dir, orig: Pos, projection: Projection): Ray | undefined {
 
@@ -216,43 +269,129 @@ export function board_pos(board: Board, pos: Pos) {
   return board.pieces.get(pos)
 }
 
-export function board_slides(board: Board) {
-  return mapmap(board.pieces, (pos, piece) => {
-    if (isPiece(piece)) {
-      return rays[piece.role].get(pos)?.map(ray => {
+export function situation_slides(situation: Situation) {
+  let { board, turn } = situation
 
+  let slide_actions = posactions<Slide>()
+  let pawn_pushes = posactions<PawnPush>()
+
+  mapmap(board.pieces, (pos, piece) => {
+    if (isPiece(piece)) {
+      rays[piece.role].get(pos)!.map(ray => {
         let { orig, dest } = ray
         let blocks = ray.between.flatMap(_ =>
           (board_pos(board, _) && _) || [])
-        let capture = board_pos(board, ray.dest) && ray.dest
+        let capturePiece = board_pos(board, ray.dest)
+        let capture = capturePiece && ray.dest
 
-        return {
+        let valid_turn = piece.color === turn
+        let valid_ok = blocks.length === 0 &&
+          (!capturePiece || capturePiece.color !== piece.color)
+        let valid_king = false
+          
+        posactions_add(slide_actions, {
+          action: 'slide', 
           orig,
           dest,
           blocks,
-          capture
-        }
+          capture,
+          valid_turn,
+          valid_king,
+          valid_ok
+        })
+      })
+    } else {
+      pawnpush_rays(pos, piece.color).map(ray => {
+        let { orig, dest } = ray
+        let blocks = ray.between.flatMap(_ =>
+          (board_pos(board, _) && _) || [])
+        let capturePiece = board_pos(board, ray.dest)
+        let capture = capturePiece && ray.dest
+
+        let valid_turn = piece.color === turn
+        let valid_ok = blocks.length === 0 &&
+          !capturePiece
+        let valid_king = false
+
+        posactions_add(slide_actions, {
+          action: 'slide', 
+          orig,
+          dest,
+          blocks,
+          capture,
+          valid_turn,
+          valid_king,
+          valid_ok
+        })
       })
     } 
-  }) 
-}
-
-
-export function board_pawn_pushes(board: Board) {
-  return mapmap(board.pieces, (pos, piece) => {
-    if (!isPiece(piece)) {
-
-    }
   })
+  return slide_actions
 }
 
-export function board_actor_valid(board: Board, turn: Color, hasorig: HasOrigDest) {
-  let actor = board_pos(board, hasorig.orig)
-  return actor?.color === turn
+export function situation_moves(situation: Situation) {
+  return posactions_filter(situation_slides(situation))
 }
 
-export function slide_valid(slide: Slide) {
-  return slide.blocks.length === 0
+export function valid_action_filter(a: IsAction) {
+  return a.valid_turn && a.valid_ok
+}
+
+export function posactions_filter<A extends IsAction>(posaction: PosActions<A>, filter: (_: IsAction) => boolean = valid_action_filter) {
+  maparray_filter(posaction.byorig, filter)
+  maparray_filter(posaction.bydest, filter)
+  maparray_filter(posaction.byblocks, filter)
+  maparray_filter(posaction.bycapture, filter)
+  return posaction
+}
+
+export function maparray_filter<K, V>(map: Map<K, Array<V>>, filter: (v: V) => boolean) {
+  for (let [key, value] of map) {
+    map.set(key, value.filter(filter))
+  }
+}
+
+export function mapfilter<K, V>(map: Map<K, V>, filter: (v: V) => boolean) {
+  for (let [key, value] of map) {
+    if (!filter(value)) {
+      map.delete(key)
+    }
+  }
+}
+
+export function posactions_add<A extends HasAction>(posaction: PosActions<A>, has_action: A) {
+  if (isSlide(has_action)) {
+    maparray_push(posaction.byorig, has_action.orig, has_action)
+    maparray_push(posaction.bydest, has_action.dest, has_action)
+
+    has_action.blocks.forEach(block =>
+      maparray_push(posaction.byblocks, block, has_action))
+
+    if (has_action.capture) {
+      maparray_push(posaction.bycapture, has_action.capture, has_action)
+    }
+  }
+}
+
+export function maparray_push<K, V>(_map: Map<K, Array<V>>, key: K, value: V) {
+  let _arr = _map.get(key) || []
+  _arr.push(value)
+  _map.set(key, _arr)
+}
+
+
+export function posactions<A extends HasAction>(): PosActions<A> {
+  let byorig = new Map(),
+    bydest = new Map(),
+    byblocks = new Map(),
+    bycapture = new Map()
+
+  return {
+    byorig,
+    bydest,
+    byblocks,
+    bycapture
+  }
 }
 
 
@@ -323,7 +462,7 @@ export function fen_situation(fen: Fen): Situation | undefined {
 }
 export const initial_situation = fen_situation(initial)!
 
-export function mapmap<K, A, B>(obj: Map<K, A>, fn: (k: K, a: A) => B): Map<K, B> {
+export function mapmap<K, A, B>(obj: Map<K, A>, fn: (k: K, a: A) => B | undefined): Map<K, B> {
   let res = new Map<K, B>()
   for (let [key, value] of obj) {
     let v2 = fn(key, value)
