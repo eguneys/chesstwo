72554626fe092961354ac92321a1842ea9029e3d
Emre Guneyler <eguneys@gmail.com>
2022-01-12 13:23:11 +0000

    binds

---
+++ README.md
@@ -0,0 +1,24 @@
+B N q  q n
+N n
+- B q / Bxq
+
+
+R b  r b  b a  Q a N q  N c k 
+Rxb
+- rxR / Q a
+
+
+B q  r Q  r k  n r
+Qxr
+- nxQ / Bxq
+
+
+k.de  N e q  N d
+Q d k
+- k e / N e
+- qd / Nxd
+
+
+k.ac  R c a  Q B c  Bc k
+Bc k
+- k a / Q c  R a
--- src/query.ts
+++ src/query.ts
@@ -1,59 +1,105 @@
-import { Fen, uci_piece, PieceOrPawn, Situation, fen_situation } from './types'
+import { rays, board_pos, color_opposite, uci_role, RayRole, posmap, Pos, Fen, uci_piece, PieceOrPawn, Situation, fen_situation } from './types'
 
 type Bind = string
 
-
 type Slide = {
+  role: RayRole,
+  slide: Array<Bind>
 }
 
-type Rule = any
-
-function slide(slide: Slide, situation: Situation) {
-  return true
+type Flee = {
+  actor: Bind,
+  flee: Array<Bind>
 }
 
-function pass(rule: Rule, situation: Situation) {
-  if ("slides" in rule) {
-    return slide(rule, situation)
+type BindMap = Map<Bind, Array<Pos>>
+
+type Rule = Slide | Flee
+
+
+
+'R r a k c  k~ac'
+
+const ALL_POS = [...posmap.values()]
+
+function slide(binds: BindMap, situation: Situation, _slide: Slide) {
+  let { slide, role } = _slide
+
+  slide.map(bind => {
+    if (!binds.has(bind)) {
+      let poss = ALL_POS.filter(pos => {
+        let role = uci_role(bind)
+        if (!role) {
+          return true
+        }
+        let color = (bind.toUpperCase() === bind) ? situation.turn : color_opposite(situation.turn)
+
+        let piece = board_pos(situation.board, pos)
+
+        if (piece) {
+          return piece.color === color && piece.role === role
+        }
+      })
+      binds.set(bind, poss)
+    }
+  })
+
+  
+
+  binds.get(slide[0])!.map(orig =>
+    rays[role].get(orig)!
+    .filter(ray =>
+      binds.get(slide[slide.length - 1])!.map(dest =>
+        dest
+      )
+    )
+    .map(ray => {
+
+
+    })
+  )
+
+}
+
+function pass(binds: BindMap, situation: Situation, rule: Rule) {
+  if ("slide" in rule) {
+    slide(binds, situation, rule)
   } else {
-    return true
   }
 }
 
 
 
 'R r a k c  k~ac'
-function str_rule(rule: string) {
-  let actor = uci_piece(rule[0])
-  if (!actor) return undefined
+function str_rule(rule: string): Rule | undefined {
 
   let space = rule[1]
 
   if (space === '~') {
+    let actor = rule[0]
 
-    let flees = rule
+    let flee = rule
       .slice(2)
       .split('')
       .map(_ => _)
 
     return {
       actor,
-      flees
+      flee
     }
   }
 
 
   if (space === ' ') {
-    let slides = rule
-      .slice(2)
+    let role = uci_role(rule[0])
+    if (!role || role === 'p') { return undefined }
+
+    let slide = rule
       .split(' ')
-      .map(_ => 
-        uci_piece(_) || _
-      )
 
       return {
-        actor,
-        slides
+        role,
+        slide
       }
   }
 }
@@ -63,15 +109,19 @@ export function qh(fen: Fen, query: string) {
   const situation = fen_situation(fen)
   if (!situation) { return false }
 
+  let binds = new Map()
+
   let lines = query.split('\n')
 
   let rules = lines[0].split('  ')
 
-  let res = rules.flatMap(rule => {
+  rules.map(rule => {
     let res = str_rule(rule)
-    if (!res) return []
-    return pass(res, situation)?[1]:[]
+    if (res) {
+      pass(binds, situation, res)
+    }
   })
 
-  return res.length === rules.length
+  console.log(binds)
+  return false
 }
